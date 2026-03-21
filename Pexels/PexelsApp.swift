bff7fb04ade6d3b0e8513ddf453e00abbb1b5315
shubham <gkumar.shubham9@gmail.com>
2026-03-22 00:54:44 +0530

    Initial Commit

---
+++ Pexels/PexelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PexelsApp.swift
+//  Pexels
+//
+//  Created by shubham gupta on 22/03/26.
+//
+
+import SwiftUI
+
+@main
+struct PexelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
